549a048cd577b0f415fb219b74323c1f13e2d534
chaitanya-911 <chaitanyar.23.beai@acharya.ac.in>
2024-09-23 18:53:39 +0530

    Add files via upload

---
+++ class data type.R
@@ -0,0 +1,20 @@
+print("chaitanya")
+print("1AY23AI029")
+print("CGPA8.5")
+#This is a program
+a=10
+b<-5
+7->c
+d=a+b
+help("reserved")
+#numeric
+a=1
+class(a)
+typeof(a)
+#integer
+class(b)
+typeof(b)
+e='H'
+class(e)
+typeof(e)
+      
